b36ebb4383eb612c8e7767af961bf2079c82c26d
juli3yu <julie.yu0913@gmail.com>
2018-01-31 22:34:44 -0500

    A1P1LastTryBeforeICry

--- Assignment 1/A1_Problem1.R
+++ Assignment 1/A1_Problem1.R
@@ -21,10 +21,10 @@ for(i in 1:n){
   }
 }
 
-bvec = c(rep(1, n), rep(1, n), rep(1, n*k), rep(0, n*k), rep(0, n*k), rep(0, k), rep(1, n), rep(1, n))
-dir = c(rep("<=", n), rep("<=", n),  rep("<=", n*k), rep(">=", n*k), rep(">=", n*k), rep("=", k), rep("=", n), rep("=", n))
+bvec = c(rep(1, n), rep(1, n), rep(1, n*k), rep(0, n*k), rep(0, n*k), rep(0, k), rep(1, n*k), rep(1, n*k), 8, rep(1, n*k), rep(1, n*k), rep(0, n*k), rep(0, 6) )
+dir = c(rep("<=", n), rep("<=", n),  rep("<=", n*k), rep(">=", n*k), rep(">=", n*k), rep("=", k), rep("<=", n*k), rep("<=", n*k), "=", rep("<=", n*k), rep("<=", n*k), rep("=", n*k), rep("=", 6))
 
-Amat = matrix(0, nrow=(n*2+(n*k)*3+k+n*2), ncol=(n*n*k + n*k*2))
+Amat = matrix(0, nrow=(n*2+(n*k)*8+k+7), ncol=(n*n*k + n*k*2))
 #contraint 1
 for (i in 1:n) {
   Amat[i, ((i - 1) * n*k + 1):(i * n*k)] = 1
@@ -78,25 +78,130 @@ for (i in 1:(k)) {
 }
 
 #contraint 7
-offSet = 24
-offSetL = 193
-for (i in 1:(n)) {
-  Amat[i+n*2+n*k*3+k, (offSet - 23):(offSet)] = 1
-  Amat[i+n*2+n*k*3+k, offSetL:(offSetL+k-1)] = 1
-  offSet = offSet + 24
-  offSetL = offSetL + k
+Count = 1
+Count2 = 1
+for(i in 1:n){
+  for(K in 1:k){
+    Amat[Count+n*2+n*k*3+k,seq(from=Count2,by =k, length.out = n)] = 1
+    Amat[Count+n*2+n*k*3+k,216+Count] = 1
+    Count = Count + 1
+    Count2 = Count2 + 1
+  }
+  Count2 = Count2 + (n-1)*k
 }
 
 #contraint 8
-offSet = 24
-offSetF = 217
-for (i in 1:(n)) {
-  Amat[i+n*2+n*k*3+k+n, (offSet - 23):(offSet)] = 1
-  Amat[i+n*2+n*k*3+k+n, offSetL:(offSetL+k-1)] = 1
-  offSet = offSet + 24
-  offSetL = offSetL + k
+Count = 1
+Count2 = 1
+for(i in 1:n){
+  for(K in 1:k){
+    Amat[Count+n*2+n*k*4+k,seq(from=Count2,by =n*k, length.out = n)] = 1
+    Amat[Count+n*2+n*k*4+k,192+Count] = 1
+    Count = Count + 1
+    Count2 = Count2 + 1
+  }
+  #Count2 = Count2 + (n-1)*k
 }
-image(Matrix(Amat))
+
+#Constraint 9: make sure that when you sum all F and all X it equals to 8
+Amat[n*2+n*k*5+k+1,(1):(n*n*k + n*k)] = 1
+
+#Constraint 10: each crew can only have first flight once
+row = 1
+col = 1
+for (i in 1:n) {
+  for(K in 1:k){
+    Amat[row+n*2+n*k*5+k+1, seq(from=n*n*k+n*k+col,by =k, length.out = n)] = 1
+    row = row + 1
+    col = col + 1
+  }
+  col = 1
+}
+#Constraint 11: each crew can only have last flight once
+row = 1
+col = 1
+for (i in 1:n) {
+  for(K in 1:k){
+    Amat[row+n*2+n*k*6+k+1, seq(from=n*n*k+col,by =k, length.out = n)] = 1
+    row = row + 1
+    col = col + 1
+  }
+  col = 1
+}
+#Constraint 12: make sure that if a crew goes from i to j then j to something else should still be done by the same crew (His paper)
+row = 1
+offSet = 0
+for (i in 1:n) {
+  for(K in 1:k){
+    Amat[row+n*2+n*k*7+k+1, seq(row,by =n*k, length.out = n)] = 1
+    Amat[row+n*2+n*k*7+k+1, seq(row + offSet + k,by =k, length.out = n-1)] = -1
+    Amat[row+n*2+n*k*7+k+1, seq(from=row+n*n*k,by =n*k, length.out = 1)] = 1
+    Amat[row+n*2+n*k*7+k+1, seq(from=row+n*n*k+n*k,by =n*k, length.out = 1)] = -1
+    row = row + 1
+  }
+  offSet = offSet + 24
+}
+
+#constraint 13: make sure that if someone starts somewhere it also needs to end in the same city
+Crew1NotF = c(2,3,4,6,8)
+for(i in 1:length(Crew1NotF)){
+  Amat[n*2+n*k*8+k+2, n*n*k+(Crew1NotF[i]-1)*3+1] = 1
+}
+Crew2NotF = c(1,2,5,7,8)
+for(i in 1:length(Crew2NotF)){
+  Amat[n*2+n*k*8+k+3, n*n*k+(Crew2NotF[i]-1)*3+2] = 1
+}
+Crew3NotF = c(1,2,5,7,8)
+for(i in 1:length(Crew3NotF)){
+  Amat[n*2+n*k*8+k+4, n*n*k+(Crew3NotF[i]-1)*3+3] = 1
+}
+
+#Constraint 14: make sure that each crew can only start in one given city
+Crew1F = c(1,5,7)
+for(i in 1:length(Crew1F)){
+  Amat[n*2+n*k*8+k+5, n*n*k+(Crew1F[i]-1)*3+1] = 1
+}
+Crew1L = c(3,4,8)
+for(i in 1:length(Crew1L)){
+  Amat[n*2+n*k*8+k+5, n*n*k+n*k+(Crew1L[i]-1)*3+1] = 1
+}
+Crew2F = c(3,4,6)
+for(i in 1:length(Crew2F)){
+  Amat[n*2+n*k*8+k+6, n*n*k+(Crew2F[i]-1)*3+2] = 1
+}
+Crew2L = c(1,2,5)
+for(i in 1:length(Crew2L)){
+  Amat[n*2+n*k*8+k+6, n*n*k+n*k+(Crew2L[i]-1)*3+2] = 1
+}
+Crew3F = c(2,8)
+for(i in 1:length(Crew3F)){
+  Amat[n*2+n*k*8+k+7, n*n*k+(Crew3F[i]-1)*3+3] = 1
+}
+Crew3L = c(6,7)
+for(i in 1:length(Crew3L)){
+  Amat[n*2+n*k*8+k+7, n*n*k+n*k+(Crew3L[i]-1)*3+3] = 1
+}
+
+# #before
+# offSet = 24
+# offSetL = 193
+# for (i in 1:(n)) {
+#   Amat[i+n*2+n*k*3+k, (offSet - 23):(offSet)] = 1
+#   Amat[i+n*2+n*k*3+k, offSetL:(offSetL+k-1)] = 1
+#   offSet = offSet + 24
+#   offSetL = offSetL + k
+# }
+# 
+# #contraint 8
+# offSet = 24
+# offSetF = 217
+# for (i in 1:(n)) {
+#   Amat[i+n*2+n*k*3+k+n, (offSet - 23):(offSet)] = 1
+#   Amat[i+n*2+n*k*3+k+n, offSetL:(offSetL+k-1)] = 1
+#   offSet = offSet + 24
+#   offSetL = offSetL + k
+# }
+ image(Matrix(Amat))
 
 
 myLP = list()
