6a6e5b337cde5351e9cfc3847b49950d66637986
unknown <stefannopolo@yahoo.com.br>
2018-03-06 22:36:50 -0500

    Problem 3 MP Fucked

--- Assignment 2/A2_Problem3.R
+++ Assignment 2/A2_Problem3.R
@@ -1,12 +1,12 @@
 library("gurobi")
 library("Matrix")
 
-L1 = 0.95
-L2 = 0.92
+L1 = 1
+L2 = 1
 
 ### Iteration 1 ###
-u1 = 100
-u2 = 100
+u1 = 0
+u2 = 0
 
 ### Subproblem 1 ###
 
@@ -22,8 +22,6 @@ mysol = gurobi(mySP1)
 x1.h = mysol$x
 z.SP1 = mysol$objval
 
-LB1 = z.SP1 - 4000*u1 - 3000*u2
-
 ### Subproblem 2 ###
 
 mySP2 = list()
@@ -37,8 +35,6 @@ mySP2$vtypes = "I"
 mysol = gurobi(mySP2)
 x2.h = mysol$x
 z.SP2 = mysol$objval
-
-LB2 = z.SP2 - 4000*u1 - 3000*u2
 
 ### Subproblem 3 ###
 
@@ -54,6 +50,32 @@ mysol = gurobi(mySP3)
 x3.h = mysol$x
 z.SP3 = mysol$objval
 
-LB3 = z.SP3 - 4000*u1 - 3000*u2
+LB = z.SP1 +z.SP2 + z.SP3 - 4000*u1 - 3000*u2
 
+### Master Problem ###
 
+myMP = list()
+myMP$modelsense = "max"
+myMP$obj = c(1,1,1,-4000,-3000)
+myMP$A = Matrix(c(1,0,0,-(x1.h[1] + x1.h[3]),-(x1.h[2] + x1.h[4]),
+                  0,1,0,-(x2.h[1] + x2.h[3]),-(x2.h[2] + x2.h[4]),
+                  0,0,1,-(x3.h[1] + x3.h[3]),-(x3.h[2] + x3.h[4])),
+                  nrow=3, ncol=5, byrow=T, sparse=T)
+myMP$sense = c("<=","<=","<=")
+myMP$rhs = c(15.7*x1.h[1] + 10.7*x1.h[2] + 15.4*x1.h[3] + 10.4*x1.h[4],
+             15.5*x1.h[1] + 10.5*x1.h[2] + 15.9*x1.h[3] + 10.9*x1.h[4],
+             15.7*x1.h[1] + 10.7*x1.h[2] + 15.4*x1.h[3] + 10.4*x1.h[4])
+myMP$vtypes = "C"
+myMP$lb = c(-Inf, -Inf, -Inf, 0, 0)
+
+mysol = gurobi(myMP)
+
+theta1 = mysol$x[1]
+theta2 = mysol$x[2]
+theta3 = mysol$x[3]
+u1 = mysol$x[4]
+u2 = mysol$x[5]
+
+UB = mysol$objval
+
+check = (LB==UB)
